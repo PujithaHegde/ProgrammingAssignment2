5daf560cc76b220877f14670f6a9a955db10b292
PujithaHegde <pujitha_hegde@hotmail.com>
2016-12-23 15:10:13 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -49,3 +49,56 @@ cacheSolve <- function(x, ...)
      x$setInverse(invMatrix)
      invMatrix
 }
+
+#############################################################################################################################
+################################################## Testing the Code #########################################################
+
+#### Example 1 ####
+
+## Inverse Matrix is not set to any value but set to NULL and hence cacheSolve function calculates the inverse of the matrix
+## which is given as an input to the makeCacheMatrix function
+             
+> source("cacheMatrix.R")
+> myMatrix <- makeCacheMatrix(matrix(c(7,13,15,17,32,43,2,4,8), 3, 3))
+> myMatrix$getMatrix()
+     [,1] [,2] [,3]
+[1,]    7   17    2
+[2,]   13   32    4
+[3,]   15   43    8
+> myMatrix$getInverse()
+NULL
+> cacheSolve(myMatrix)
+      [,1] [,2] [,3]
+[1,] -42.0   25 -2.0
+[2,]  22.0  -13  1.0
+[3,] -39.5   23 -1.5
+> myMatrix$getInverse()
+      [,1] [,2] [,3]
+[1,] -42.0   25 -2.0
+[2,]  22.0  -13  1.0
+[3,] -39.5   23 -1.5
+
+#### Example 2 ####
+
+## Inverse Matrix is set to some value in the makeCacheMatrix function and hence cacheSolve function retreives the cached 
+## data (inverse matrix value from the makeCacheMatrix function)
+             
+> source("cacheMatrix.R")
+> myMatrix$setMatrix(matrix(c(7,13,15,17,32,43,2,4,8), 3, 3))
+> myMatrix$getMatrix()
+     [,1] [,2] [,3]
+[1,]    7   17    2
+[2,]   13   32    4
+[3,]   15   43    8
+> myMatrix$setInverse(matrix(c(-42, 22, -39.5, 25, -13, 23, -2, 1, -1.5), 3, 3))
+> myMatrix$getInverse()
+      [,1] [,2] [,3]
+[1,] -42.0   25 -2.0
+[2,]  22.0  -13  1.0
+[3,] -39.5   23 -1.5
+> cacheSolve(myMatrix)
+getting cached data
+      [,1] [,2] [,3]
+[1,] -42.0   25 -2.0
+[2,]  22.0  -13  1.0
+[3,] -39.5   23 -1.5
